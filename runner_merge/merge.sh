d1fec0feac254bb31c0ddeeff77aca79b795552f
Xavier León <109275656+xleon-seidor-es@users.noreply.github.com>
2023-11-20 15:08:29 +0100

    ref: rename merge variables

---
+++ runner_merge/merge.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Usage:
+# ./merge.sh src=<SOURCE_BRANCH> dest=<dest_branch>
+SOURCE_BRANCH=$(echo $*|sed -s 's/ /\n/g'|grep -e '^source_branch_name='|cut -d= -f2)
+TARGET_BRANCH=$(echo $*|sed -s 's/ /\n/g'|grep -e '^target_branch_name='|cut -d= -f2)
+CUSTOM_DIR="/opt/cxcommerce/custom-cx"
+
+if [ -z "${SOURCE_BRANCH}" ] || [ -z "${TARGET_BRANCH}" ]; then
+    echo "Usage: ./merge.sh source_branch_name=<SOURCE_BRANCH> target_branch_name=<dest_branch>"
+    exit 1
+fi
+
+cd ${CUSTOM_DIR}
+git fetch --all
+git checkout ${TARGET_BRANCH}
+git pull
+git checkout ${SOURCE_BRANCH}
+git pull
+git merge ${TARGET_BRANCH}
