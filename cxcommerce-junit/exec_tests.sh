3f378183bf4e82f6dd7c35e7d4db309232d5a8fc
Xavier León <109275656+xleon-seidor-es@users.noreply.github.com>
2023-12-14 11:11:18 +0100

    fix: add CUSTOM_DIR on unit_test action

--- cxcommerce-junit/exec_tests.sh
+++ cxcommerce-junit/exec_tests.sh
@@ -4,9 +4,9 @@ set -e
 ##This is a general variables file for the CI/CD pipeline
 CXCOMMERCE_HOME=/opt/cxcommerce
 PLATFORM_DIR=/opt/cxcommerce/hybris/bin/platform
-PROJECT_DIR=/opt/cxcommerce/custom-cx
 
 CUSTOM_MODULES_DIR=$(echo $*|sed -s 's/ /\n/g'|grep -e '^custom_modules_dir='|cut -d= -f2)
+CUSTOM_DIR=$(echo $*|sed -s 's/ /\n/g'|grep -e '^custom_dir='|cut -d= -f2)
 
 if [ -z "${CUSTOM_MODULES_DIR}" ] ; then
     echo "Usage: ./exec_tests.sh custom_modules_dir=${{ inputs.CUSTOM_MODULES_DIR }}"
@@ -16,8 +16,8 @@ fi
 cd ${PLATFORM_DIR}
 source setantenv.sh
 ant initialize -Dtenant=junit
-extensions_list=$(find ${PROJECT_DIR}/${CUSTOM_MODULES_DIR}/* -mindepth 1 -maxdepth 1 -type d ! -regex '.*cicd.*' ! -regex '.*sampledata.*' ! -regex '.*external.*' -exec basename {} \;)
+extensions_list=$(find ${CUSTOM_DIR}/${CUSTOM_MODULES_DIR}/* -mindepth 1 -maxdepth 1 -type d ! -regex '.*cicd.*' ! -regex '.*sampledata.*' ! -regex '.*external.*' -exec basename {} \;)
 ant jacocoalltests -Dtestclasses.extensions="$(echo $extensions_list|sed -s 's/ /,/g')"
 cd ${PLATFORM_DIR}
 ant jacocoreport
-cd ${PROJECT_DIR}
+cd ${CUSTOM_DIR}
