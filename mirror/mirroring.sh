5a6f8da3f4bacc65d44a61328b8cae94094c159e
Xavier León <109275656+xleon-seidor-es@users.noreply.github.com>
2023-12-20 10:00:10 +0100

    feat: add mirror action

---
+++ mirror/mirroring.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+source_repo=$(echo $*|sed -s 's/ /\n/g'|grep -e '^source_repo='|cut -d= -f2)
+target_repo=$(echo $*|sed -s 's/ /\n/g'|grep -e '^target_repo='|cut -d= -f2)
+lfs_enabled=$(echo $*|sed -s 's/ /\n/g'|grep -e '^lfs_enabled='|cut -d= -f2)
+old_path=$(echo ${source_repo}|awk -F '/' '{print $NF}'|cut -d '.' -f1)
+
+if [ -z "${source_repo}" ] || [ -z "${target_repo}"] ; then
+    echo "Usage: ./mirroring.sh [lfs_enabled=1] source_repo=git@github.com/seidor-cx/repo.git target_repo=git@gitlab.com:group/repo.git"
+    echo "lfs_enabled is optional, default is 0"
+    exit 1
+fi
+if [ -z "${lfs_enabled}" ] ; then
+    lfs_enabled=0
+fi
+
+git clone --bare ${source_repo}
+cd ${old_path}
+if [ ${lfs_enabled} -eq 1 ]; then
+    git lfs fetch --all
+fi
+git push --mirror ${target_repo}
+if [ ${lfs_enabled} -eq 1 ]; then
+    git lfs push --all ${target_repo}
+fi
