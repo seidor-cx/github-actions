894572bf15614073fb52364ee8eb7c4d9c185d51
Xavier León <109275656+xleon-seidor-es@users.noreply.github.com>
2023-12-27 13:13:57 +0100

    debug: test debug

--- cxcommerce-build/build.sh
+++ cxcommerce-build/build.sh
@@ -5,7 +5,7 @@ PLATFORM_DIR=/opt/cxcommerce/hybris/bin/platform
 INITIALIZE=$(echo $*|sed -s 's/ /\n/g'|grep -e '^initialize='|cut -d= -f2)
 CUSTOM_DIR=$(echo $*|sed -s 's/ /\n/g'|grep -e '^custom_dir='|cut -d= -f2)
 ENVIRONMENT=$(echo $*|sed -s 's/ /\n/g'|grep -e '^environment='|cut -d= -f2)
-AWK_PARSER="$(pwd)/parse_initialize_errors.awk"
+AWK_PARSER="$GITHUB_ACTION_PATH/parse_initialize_errors.awk"
 
 if [ -z "${INITIALIZE}" ] ; then
     echo "Usage: ./build_cxcommerce.sh initialize=<0|1> custom_dir=${{ inputs.CUSTOM_DIR }} environment=${{ inputs.ENVIRONMENT }}"
