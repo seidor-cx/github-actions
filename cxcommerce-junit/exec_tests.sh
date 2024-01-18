d93eb74b859a5b7da6258021eb3cb83e5a9d6e14
Xavier León <109275656+xleon-seidor-es@users.noreply.github.com>
2024-01-18 15:51:05 +0100

    rest: delete debug messages

--- cxcommerce-junit/exec_tests.sh
+++ cxcommerce-junit/exec_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -ex
+set -e
 
 ##This is a general variables file for the CI/CD pipeline
 CXCOMMERCE_HOME=/opt/cxcommerce
@@ -25,16 +25,10 @@ ant initialize -Dtenant=junit
 echo "Load extensions list"
 echo "Get list of directories on ${CUSTOM_DIR}/${CUSTOM_MODULES_DIR}"
 cd ${CUSTOM_DIR}/${CUSTOM_MODULES_DIR}
-ls -1 --color=never|sort
 ls -1 --color=never|sort > ${LIST_EXTENSIONS_DIR}
 echo "Get list of extensions on localextensions.xml"
-cat ${CXCOMMERCE_HOME}/hybris/config/localextensions.xml |grep name|grep -v '\-\->'|cut -d "=" -f2|sed 's/"//g'|sed 's/ \/>//g'|sed "s/'//g"|sed 's/\/>//g'
 cat ${CXCOMMERCE_HOME}/hybris/config/localextensions.xml |grep name|grep -v '\-\->'|cut -d "=" -f2|sed 's/"//g'|sed 's/ \/>//g'|sed "s/'//g"|sed 's/\/>//g'|sort > ${LIST_EXTENSIONS_ACTIVE}
 echo "Save the intersection between localextensions and directories on ${CUSTOM_DIR}/${CUSTOM_MODULES_DIR}"
-echo "DEBUG: show content from ${LIST_EXTENSIONS_ACTIVE}"
-cat ${LIST_EXTENSIONS_ACTIVE}
-echo "DEBUG: show content from ${LIST_EXTENSIONS_DIR}"
-cat ${LIST_EXTENSIONS_DIR}
 extensions_list=$(comm -12 ${LIST_EXTENSIONS_DIR} ${LIST_EXTENSIONS_ACTIVE} |grep -v 'cicd' |grep -v 'sampledata' |grep -v 'external' |grep -v 'mirakl' |grep -v 'test')
 echo "Extensions list: ${extensions_list}"
 echo "Run tests"
