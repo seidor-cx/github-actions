f41dbfaa1c327f42c1217426a2152f4e40eec362
Xavier León <109275656+xleon-seidor-es@users.noreply.github.com>
2024-01-18 12:23:57 +0100

    deb: add debug messages

--- cxcommerce-junit/exec_tests.sh
+++ cxcommerce-junit/exec_tests.sh
@@ -4,9 +4,12 @@ set -ex
 ##This is a general variables file for the CI/CD pipeline
 CXCOMMERCE_HOME=/opt/cxcommerce
 PLATFORM_DIR=/opt/cxcommerce/hybris/bin/platform
+LIST_EXTENSIONS_DIR=/tmp/list_directories
+LIST_EXTENSIONS_ACTIVE=/tmp/list_extensions
 
 CUSTOM_MODULES_DIR=$(echo $*|sed -s 's/ /\n/g'|grep -e '^custom_modules_dir='|cut -d= -f2)
 CUSTOM_DIR=$(echo $*|sed -s 's/ /\n/g'|grep -e '^custom_dir='|cut -d= -f2)
+
 
 if [ -z "${CUSTOM_MODULES_DIR}" ] ; then
     echo "Usage: ./exec_tests.sh custom_modules_dir=${{ inputs.CUSTOM_MODULES_DIR }}"
@@ -22,11 +25,15 @@ ant initialize -Dtenant=junit
 echo "Load extensions list"
 echo "Get list of directories on ${CXCOMMERCE_HOME}/${CUSTOM_MODULES_DIR}"
 cd ${CXCOMMERCE_HOME}/${CUSTOM_MODULES_DIR}
-ls -1 --color=never|sort > /tmp/list_directories
+ls -1 --color=never|sort > ${LIST_EXTENSIONS_DIR}
 echo "Get list of extensions on localextensions.xml"
-cat ${CXCOMMERCE_HOME}/hybris/config/localextensions.xml |grep name|grep -v '\-\->'|cut -d "=" -f2|sed 's/"//g'|sed 's/ \/>//g'|sed "s/'//g"|sed 's/\/>//g'|sort > /tmp/list_extensions
+cat ${CXCOMMERCE_HOME}/hybris/config/localextensions.xml |grep name|grep -v '\-\->'|cut -d "=" -f2|sed 's/"//g'|sed 's/ \/>//g'|sed "s/'//g"|sed 's/\/>//g'|sort > ${LIST_EXTENSIONS_ACTIVE}
 echo "Save the intersection between localextensions and directories on ${CXCOMMERCE_HOME}/${CUSTOM_MODULES_DIR}"
-extensions_list=$(comm -12 /tmp/list_directories /tmp/list_extensions |grep -v 'cicd' |grep -v 'sampledata' |grep -v 'external' |grep -v 'mirakl' |grep -v 'test')
+echo "DEBUG: show content from ${LIST_EXTENSIONS_WORKS}"
+cat ${LIST_EXTENSIONS_WORKS}
+echo "DEBUG: show content from ${LIST_EXTENSIONS_DIR}"
+cat ${LIST_EXTENSIONS_DIR}
+extensions_list=$(comm -12 ${LIST_EXTENSIONS_DIR} ${LIST_EXTENSIONS_ACTIVE} |grep -v 'cicd' |grep -v 'sampledata' |grep -v 'external' |grep -v 'mirakl' |grep -v 'test')
 echo "Extensions list: ${extensions_list}"
 echo "Run tests"
 cd ${PLATFORM_DIR}
