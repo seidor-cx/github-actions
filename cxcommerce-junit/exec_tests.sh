73f26fc54908ac419452e546a89c21c7f106ddd1
Xavier León <109275656+xleon-seidor-es@users.noreply.github.com>
2024-01-18 15:19:23 +0100

    fix: delete CXCOMMERCE_HOME variable from custom_modules_dir, was duplicated

--- cxcommerce-junit/exec_tests.sh
+++ cxcommerce-junit/exec_tests.sh
@@ -23,14 +23,14 @@ source setantenv.sh
 echo "Initialize tenant Junit"
 ant initialize -Dtenant=junit
 echo "Load extensions list"
-echo "Get list of directories on ${CXCOMMERCE_HOME}/${CUSTOM_DIR}/${CUSTOM_MODULES_DIR}"
-cd ${CXCOMMERCE_HOME}/${CUSTOM_DIR}/${CUSTOM_MODULES_DIR}
+echo "Get list of directories on ${CUSTOM_DIR}/${CUSTOM_MODULES_DIR}"
+cd ${CUSTOM_DIR}/${CUSTOM_MODULES_DIR}
 ls -1 --color=never|sort
 ls -1 --color=never|sort > ${LIST_EXTENSIONS_DIR}
 echo "Get list of extensions on localextensions.xml"
 cat ${CXCOMMERCE_HOME}/hybris/config/localextensions.xml |grep name|grep -v '\-\->'|cut -d "=" -f2|sed 's/"//g'|sed 's/ \/>//g'|sed "s/'//g"|sed 's/\/>//g'
 cat ${CXCOMMERCE_HOME}/hybris/config/localextensions.xml |grep name|grep -v '\-\->'|cut -d "=" -f2|sed 's/"//g'|sed 's/ \/>//g'|sed "s/'//g"|sed 's/\/>//g'|sort > ${LIST_EXTENSIONS_ACTIVE}
-echo "Save the intersection between localextensions and directories on ${CXCOMMERCE_HOME}/${CUSTOM_DIR}/${CUSTOM_MODULES_DIR}"
+echo "Save the intersection between localextensions and directories on ${CUSTOM_DIR}/${CUSTOM_MODULES_DIR}"
 echo "DEBUG: show content from ${LIST_EXTENSIONS_ACTIVE}"
 cat ${LIST_EXTENSIONS_ACTIVE}
 echo "DEBUG: show content from ${LIST_EXTENSIONS_DIR}"
