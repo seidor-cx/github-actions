4f4bb59a67b3a40bb518ccb485c2291d7656bcf5
Xavier León <109275656+xleon-seidor-es@users.noreply.github.com>
2024-01-18 10:43:50 +0100

    fix: change get list directories

--- cxcommerce-junit/exec_tests.sh
+++ cxcommerce-junit/exec_tests.sh
@@ -20,16 +20,16 @@ source setantenv.sh
 echo "Initialize tenant Junit"
 ant initialize -Dtenant=junit
 echo "Load extensions list"
-echo "Get list of directories on ${CUSTOM_MODULES_DIR}"
-cd ${CUSTOM_MODULES_DIR}
+echo "Get list of directories on ${CXCOMMERCE_HOME}/${CUSTOM_MODULES_DIR}"
+cd ${CXCOMMERCE_HOME}/${CUSTOM_MODULES_DIR}
 ls -1 --color=never|sort > /tmp/list_directories
-cd -
 echo "Get list of extensions on localextensions.xml"
 cat ${CXCOMMERCE_HOME}/hybris/config/localextensions.xml |grep name|grep -v '\-\->'|cut -d "=" -f2|sed 's/"//g'|sed 's/ \/>//g'|sed "s/'//g"|sed 's/\/>//g'|sort > /tmp/list_extensions
 echo "Save the intersection between localextensions and directories on ${CUSTOM_MODULES_DIR}"
 extensions_list=$(comm -12 /tmp/list_directories /tmp/list_extensions |grep -v 'cicd' |grep -v 'sampledata' |grep -v 'external' |grep -v 'mirakl' |grep -v 'test')
 echo "Extensions list: ${extensions_list}"
 echo "Run tests"
+cd ${PLATFORM_DIR}
 ant jacocoalltests -Dtestclasses.extensions="$(echo $extensions_list|sed -s 's/ /,/g')"
 echo "Generate Jacoco report"
 ant jacocoreport
